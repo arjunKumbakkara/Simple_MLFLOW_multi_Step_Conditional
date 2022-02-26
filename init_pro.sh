54973c4d454a28e9a7dda747041ab782f087e3f1
Arjun Kumbakkara <arjunkumbakkara@gmail.com>
2022-02-26 14:15:25 +0530

    initial Setup Script

--- init_pro.sh
+++ init_pro.sh
@@ -1,4 +1,5 @@
 conda create --prefix ./env python=3.7 -y
-source C:/Users/sunny/miniconda3/etc/profile.d/conda.sh # use your username instead of sunny
-source activate ./env
+#source C:/Users/arjunkumbakkara/miniconda3/etc/profile.d/conda.sh # use your username instead of sunny
+#source activate ./env
+conda activate ./env
 pip install -r requirements.txt
